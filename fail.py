818c6a71875399afaf1042fe93dcb1c1bd6c3d1f
Ashutosh kumar Sah <64386187+ashu-cybertron@users.noreply.github.com>
2020-05-30 11:22:42 +0530

    Delete fail.py

--- fail.py
+++
@@ -1,8 +0,0 @@
-import smtplib
-s = smtplib.SMTP('smtp.gmail.com', 587)
-s.starttls()
-s.login("email@gmail.com", "password")
-message = "Hey developer the file committed by you for training model has some flaws due to which accuracy of model is not good so some tweaks are automatically done to get better accuracy of model"
-s.sendmail("email@gmail.com", "reciever@gmail.com", message)
-print("Mail Sent")
-s.quit() 
