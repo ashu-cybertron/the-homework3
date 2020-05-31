e2de77c87ff5f8480a83d8c6b4bb5b12bbc67281
Ashutosh kumar Sah <64386187+ashu-cybertron@users.noreply.github.com>
2020-05-31 12:26:12 +0530

    Add files via upload

---
+++ success.py
@@ -0,0 +1,8 @@
+import smtplib
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+s.login(fanofbitlalpur@gmail.com", "jaishreeram123@")
+message = "Hi ashutosh your model is ready and giving best accuracy"
+s.sendmail("fanofbitlalpur@gmail.com", "anandashutosh803@gmail.com", message)
+print("Mail Sent")
+s.quit() 
